041ed8e7c69a29a1881ba360b98ffbcdfbbf73ce
Nate Minshew <nate.minshew@asynchrony.com>
2014-08-22 13:18:29 -0500

    Added static library for including in other projects.

---
+++ MasonryLib/MasonryLib/MasonryLib.h
@@ -0,0 +1,13 @@
+//
+//  MasonryLib.h
+//  MasonryLib
+//
+//  Created by Nate Minshew on 8/22/14.
+//  Copyright (c) 2014 Jonas Budelmann. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface MasonryLib : NSObject
+
+@end
---
+++ MasonryLib/MasonryLib/MasonryLib.m
@@ -0,0 +1,13 @@
+//
+//  MasonryLib.m
+//  MasonryLib
+//
+//  Created by Nate Minshew on 8/22/14.
+//  Copyright (c) 2014 Jonas Budelmann. All rights reserved.
+//
+
+#import "MasonryLib.h"
+
+@implementation MasonryLib
+
+@end
